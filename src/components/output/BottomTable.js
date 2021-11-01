a53d7688f97cfb826e7d0471e412475c6deee7af
Abdulvokhid <mr.ochilov1997@gmail.com>
2021-11-01 23:42:30 +0900

    form change

--- src/components/output/BottomTable.js
+++ src/components/output/BottomTable.js
@@ -1,76 +1,76 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
-const BottomTable = () => {
-  const [length, setLength] = useState(3);
+// const BottomTable = () => {
+//   const [length, setLength] = useState(3);
 
-  const TableBody = [];
+//   const TableBody = [];
 
-  for (let i = 0; i < length; i++) {
-    TableBody.push(
-      <tr key={i}>
-        <td>
-          <input type="text" name="bl_number" />
-        </td>
-        <td>
-          <input type="text" name="client" />
-        </td>
-        <td>
-          <input type="text" name="total_quantity" />
-        </td>
-        <td>
-          <input type="text" name="quantity" />
-        </td>
-      </tr>
-    );
-  }
+//   for (let i = 0; i < length; i++) {
+//     TableBody.push(
+//       <tr key={i}>
+//         <td>
+//           <input type="text" name="bl_number" />
+//         </td>
+//         <td>
+//           <input type="text" name="client" />
+//         </td>
+//         <td>
+//           <input type="text" name="total_quantity" />
+//         </td>
+//         <td>
+//           <input type="text" name="quantity" />
+//         </td>
+//       </tr>
+//     );
+//   }
 
-  function HandleAdd() {
-    setLength(length + 1);
-  }
+//   function HandleAdd() {
+//     setLength(length + 1);
+//   }
 
-  function HandleRemove() {
-    if (length > 3) setLength(length - 1);
-  }
+//   function HandleRemove() {
+//     if (length > 3) setLength(length - 1);
+//   }
 
-  return (
-    <>
-      <table className="bottom-table out-bottom-table">
-        <thead>
-          <tr>
-            <th>
-              <span> B/L NO. (비엘 번호)</span>
-            </th>
-            <th>
-              <span>화주명(상호)</span>
-            </th>
-            <th>
-              <span> 전체수량 </span>
-            </th>
-            <th>
-              <span> 출고수량 </span>
-            </th>
-          </tr>
-        </thead>
-        <tbody>{TableBody.map((row) => row)}</tbody>
-      </table>
-      <div className="plus-btn-cont">
-        <button
-          onClick={HandleRemove}
-          type="button"
-          className="out-minus-btn minus-btn"
-        >
-          -
-        </button>
-        <button
-          onClick={HandleAdd}
-          type="button"
-          className="out-plus-btn plus-btn"
-        >
-          +
-        </button>
-      </div>
-    </>
-  );
-};
+//   return (
+//     <>
+//       <table className="bottom-table out-bottom-table">
+//         <thead>
+//           <tr>
+//             <th>
+//               <span> B/L NO. (비엘 번호)</span>
+//             </th>
+//             <th>
+//               <span>화주명(상호)</span>
+//             </th>
+//             <th>
+//               <span> 전체수량 </span>
+//             </th>
+//             <th>
+//               <span> 출고수량 </span>
+//             </th>
+//           </tr>
+//         </thead>
+//         <tbody>{TableBody.map((row) => row)}</tbody>
+//       </table>
+//       <div className="plus-btn-cont">
+//         <button
+//           onClick={HandleRemove}
+//           type="button"
+//           className="out-minus-btn minus-btn"
+//         >
+//           -
+//         </button>
+//         <button
+//           onClick={HandleAdd}
+//           type="button"
+//           className="out-plus-btn plus-btn"
+//         >
+//           +
+//         </button>
+//       </div>
+//     </>
+//   );
+// };
 
-export default BottomTable;
+// export default BottomTable;
