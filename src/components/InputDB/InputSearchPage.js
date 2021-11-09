c6fb538f0da6c1cc26463a068b45af1563efa5da
Abdulvokhid <mr.ochilov1997@gmail.com>
2021-11-10 04:42:23 +0900

    Search Loading

--- src/components/InputDB/InputSearchPage.js
+++ src/components/InputDB/InputSearchPage.js
@@ -1,27 +1,33 @@
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../layout/Header";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import SearchForm from "./SearchForm";
 import InputTable from "./InputTable";
+import Hypnosis from "react-cssfx-loading/lib/Hypnosis";
 import { getInputHistory } from "../../api/index";
 
 const InputSearchPage = () => {
   const [inputHistory, setInputHistory] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    loading && setTimeout(() => setLoading(false), 1000);
+  }, [loading]);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     const form = event.currentTarget;
 
-    const searchData = {
-      name: form.name.value,
-      from_date: form.from_date.value,
-      to_date: form.to_date.value,
-    };
-
+    // const searchData = {
+    //   name: form.name.value,
+    //   from_date: form.from_date.value,
+    //   to_date: form.to_date.value,
+    // };
+    setLoading(true);
     const value = await getInputHistory(
       form.name.value,
       form.from_date.value,
@@ -30,7 +36,7 @@ const InputSearchPage = () => {
     setInputHistory(value.data.data);
   };
 
-  return (
+  return !loading ? (
     <>
       <Header />
       <Container>
@@ -54,6 +60,14 @@ const InputSearchPage = () => {
         {inputHistory.length > 0 && <InputTable data={inputHistory} />}
       </Container>
     </>
+  ) : (
+    <Hypnosis
+      style={{
+        position: "absolute",
+        left: "50%",
+        top: "50%",
+      }}
+    />
   );
 };
 
--- src/components/outputDB/OutputSearchPage.js
+++ src/components/outputDB/OutputSearchPage.js
@@ -1,26 +1,32 @@
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../layout/Header";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import SearchForm from "./SearchForm";
 import OutputTable from "./OutputTable";
+import Hypnosis from "react-cssfx-loading/lib/Hypnosis";
 import { getOutputHistory } from "../../api/index";
 
 const OutputSearchPage = () => {
   const [outputHistory, setOutputHistory] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    loading && setTimeout(() => setLoading(false), 1000);
+  }, [loading]);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     const form = event.currentTarget;
-    const searchData = {
-      name: form.name.value,
-      from_date: form.from_date.value,
-      to_date: form.to_date.value,
-    };
-
+    // const searchData = {
+    //   name: form.name.value,
+    //   from_date: form.from_date.value,
+    //   to_date: form.to_date.value,
+    // };
+    setLoading(true);
     const value = await getOutputHistory(
       form.name.value,
       form.from_date.value,
@@ -28,16 +34,17 @@ const OutputSearchPage = () => {
     );
     console.log(value.data.data);
     setOutputHistory(value.data.data);
+    // console.log(event);
     // console.log(outputHistory);
     // console.log(searchData);
   };
 
-  return (
+  return !loading ? (
     <>
       <Header />
       <Container>
         <h1 className="page-heading">출고 조회</h1>
-        <Form onSubmit={handleSubmit}>
+        <Form onSubmit={handleSubmit} id="output-search">
           <SearchForm />
 
           <Row className="justify-content-md-center">
@@ -56,6 +63,14 @@ const OutputSearchPage = () => {
         {outputHistory.length > 0 && <OutputTable data={outputHistory} />}
       </Container>
     </>
+  ) : (
+    <Hypnosis
+      style={{
+        position: "absolute",
+        left: "50%",
+        top: "50%",
+      }}
+    />
   );
 };
 
